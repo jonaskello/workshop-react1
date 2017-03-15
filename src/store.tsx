826f2674e3008621971c003180dcee442ab22133
Jonas Kello <jonas.kello@divid.se>
2017-03-15 10:53:23 +0100

    Refactor

--- src/store.tsx
+++ src/store.tsx
@@ -1,24 +1,19 @@
 import * as TodosReducer from "./todos/reducer";
 import * as PersonsReducer from "./persons/reducer";
-import * as RootReducer from "./root-reducer";
-
-export interface Action {
-    type: string,
-    payload?: any,
-}
+import * as RootReducer from "./reducer";
 
 let state: RootReducer.RootState = {
     todos: undefined,
     persons: undefined,
 };
 
-let callback;
+let callback: () => void;
 
 export function subscribe(c: () => void) {
     callback = c;
 }
 
-export function dispatch(action: Action) {
+export function dispatch(action: any): void {
     console.log("BEFORE", state);
     console.log("DISPATCH: ", action);
     const newState = RootReducer.reduce(getState(), action);
